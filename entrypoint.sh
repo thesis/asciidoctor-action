ad051070976a1ad3c4a441d1c78f09de0d6c2cc3
Antonio Salazar Cardozo <antonio@thesis.co>
2020-05-29 20:25:02 -0400

    Add GitHub Action to test this GitHub Action
    This will be failing for now, but the goal is to test that the commands the
    action will run will be the expected ones. It's also a bit duplicative as
    currently structured.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,44 @@
 #!/bin/bash
-# -l = login shell
-read -r OUTPUT_FORMAT ASCIIDOCTOR_ARGS INPUT_FILES <<<$*
+# Exit on error.
+set -ex
+
+echo $0 $1 $2 $3
+EX_USAGE=64 # Usage error exit code from /usr/include/sysexits.h
+
+OUTPUT_FORMAT=
+ASCIIDOCTOR_ARGS=
+
+while getopts "f:a:" arg; do
+  case "${arg}" in
+    f)
+      OUTPUT_FORMAT=${OPTARG}
+      ;;
+    a)
+      ASCIIDOCTOR_ARGS=${OPTARG}
+      ;;
+    :)
+      printf "Expected -${OPTARG} to have a value\n" >&2
+      exit $EX_USAGE
+      ;;
+    *)
+      printf "Unexpected arg ${OPTARG}\n" >&2
+      exit $EX_USAGE
+      ;;
+  esac
+done
+shift $((OPTIND-1))
+
+read -r INPUT_FILES <<<$*
 
 ASCIIDOCTOR=asciidoctor
 if [[ "pdf" = $OUTPUT_FORMAT ]]; then
   ASCIIDOCTOR=asciidoctor-pdf
 fi
 
+printf "$*\n" >&2
 if [[ -z $INPUT_FILES ]]; then
-  echo "No input files specified" 1&>2
-  exit 64 # EX_USAGE from /usr/include/sysexits.h
+  printf "No input files specified\n" >&2
+  exit $EX_USAGE
 fi
 
 # TEST env variable indicates we should be in testing mode (below).
@@ -30,7 +59,7 @@ else
     echo "Commands equal test expectations."
     exit 0
   else                                                                             
-    printf "Ran unexpected commands:\n$commands\nvs\n$TEST"                            
+    printf "Ran unexpected commands:\n$commands\nvs\n$TEST\n" >&2
     exit 1
   fi
 
